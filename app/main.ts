51ee2acea1854cfa4f2bb339474c7336e3c07224
David Pryce <dpryce@seek.com.au>
2016-10-13 09:07:36 +1100

    playing with http

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,4 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrap } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
 
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+bootstrap(AppComponent, []);
